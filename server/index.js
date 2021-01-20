2dd607bfb2b13e1942188ca0ae2b1f2fd3ea81d3
dolev146 <62290677+dolev146@users.noreply.github.com>
2021-01-20 17:27:20 +0200

    updating routing

--- server/index.js
+++ server/index.js
@@ -25,6 +25,10 @@ if (process.env.NODE_ENV === "production") {
 
 }
 
+app.get("*", (req, res) => {
+  res.redirect("/");
+});
+
 // listiner
 const port = process.env.PORT || 1000
 app.listen(port, () => console.log(`up and running on ${port}`))
